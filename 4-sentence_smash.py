bb9c63aedc4d9141524ee6c88c50a1dce7c471c8
afauzi949.AlfauziStay <afauzi949.alfauzi949@gmail.com>
2024-02-15 21:41:02 +0700

    Day 4

---
+++ 4-sentence_smash.py
@@ -0,0 +1,9 @@
+# UZI Coding Challenge
+# 4/366
+# https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/python
+
+def smash(words):
+    return " ".join(words) # " " -> separator spasi dan join untuk menggabungkan
+    
+hasil = (smash(["this", "is", "a", "really", "long", "sentence"]))
+print(hasil)
