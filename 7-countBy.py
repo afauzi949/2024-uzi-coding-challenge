edc171d93e4c0760c52cbc4e337d07a666311bfb
afauzi949.AlfauziStay <afauzi949.alfauzi949@gmail.com>
2024-02-20 21:25:38 +0700

    Day 7

---
+++ 7-countBy.py
@@ -0,0 +1,11 @@
+# UZI Coding Challenge
+# 7/366
+# https://www.codewars.com/kata/5513795bd3fafb56c200049e/train/python
+
+def countBy(x, n):
+    z = []
+    for i in range(1, n+1):
+        z.append(i * x)
+    return z
+
+print(countBy(2, 5))
